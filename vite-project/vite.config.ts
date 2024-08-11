e3f215836b534a961c760b55be07fcf89931e87e
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-08-11 15:50:45 +0700

    feat(docker-compose): configure volume + auto reload for vite project

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // This allows the server to be accessed from outside the container
+  },
   plugins: [react()],
-})
+});
